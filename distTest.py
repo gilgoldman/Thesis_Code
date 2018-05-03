b86c0d1cb606f1ed4dc5ff0225d3fe0402596fbe
RPI <gilgoldman@gmail.com>
2018-05-03 11:25:50 +0000

    Fix Distance measurement

--- distTest.py
+++ distTest.py
@@ -2,17 +2,20 @@ import RPi.GPIO as GPIO
 import time
 
 def main():
-#    for _ in xrange(5):
-        TRIG = 7
-        ECHO = 11
+#    result_list = [] # a list to be used in calculation of average
+    for _ in xrange(10):
+	TRIG = 23 # BCM value of pin 16
+	ECHO = 24 # BCM value of pin 18
+		  # Remember that ECHO needs a voltage divider
 
-        GPIO.setmode(GPIO.BOARD)
+	GPIO.setmode(GPIO.BCM)
         GPIO.setup(TRIG,GPIO.OUT)
         GPIO.setup(ECHO,GPIO.IN)
         GPIO.setwarnings(False)
 
         GPIO.output(TRIG, False)
         time.sleep(2)
+#	time.sleep(1) # Still works with 1 second delay - why use 2?
         GPIO.output(TRIG, True)
         time.sleep(0.00001)
         GPIO.output(TRIG, False)
@@ -25,10 +28,12 @@ def main():
 
         pulseDuration = pulseEnd - pulseStart
         distance = pulseDuration * 17150
-        distance = round(distance, 4)
-#        print """Distance: %d cm""" % (distance)
+        distance = round(distance, 3)
+#	result_list.append(distance) # Appends results to the list
+	print "Distance:", distance, "cm"
         GPIO.cleanup()
-       # return distance
+#    avg = (float(sum(result_list)))/(float(len(result_list))) # Calculates the measurement average
+#    print "Average distance: ", avg, "cm"
 
 if __name__ == "__main__":
     x = main()
